464fab4615e9801b54b64c06b0543b1acf0cc36d
Sam Bequette <sbb3391@gmail.com>
2020-03-19 17:07:24 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(name, default= "Ruby Programmer")
+def say_hello(name,fall_back = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 
-say_hello("Kent Beck")
+say_hello()
